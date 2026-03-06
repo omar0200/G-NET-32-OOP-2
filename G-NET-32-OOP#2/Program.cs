fe9cf7e58e1665373cfb403ee9801415da244b99
omar0200 <omarminato17@gmail.com>
2026-03-06 17:57:47 +0200

    Question 1

--- G-NET-32-OOP#2/Program.cs
+++ G-NET-32-OOP#2/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question 1
+            //    public string Owner;
+            // public double Balance; both of them are public 
+
+            //to fix it i will make it private with setter and getter and  i will do validation to withdraw function 
+
+            //exposing fields directly  is bad because it violate oop principles because any one can set and get informations which should be private 
+
+            #endregion
+
         }
     }
 }
